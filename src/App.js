f6748fe1fc4f37dd01252dcc3ec48e4298ee190a
Charles Scholle <Charles.Scholle@mikealbert.com>
2019-03-13 12:00:22 -0400

    Before starting Section 4, Lecture 50

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Person from './Person/Person';
 
 class App extends Component {
+  state = {
+    persons: [
+      { name: "Max", age: "28" },
+      { name: "Charles", age: "32" },
+      { name: "Stacy", age: "26" }
+    ]
+  }
+
+  switchNameHandler = (newAge) => {
+    console.log('switchNameHandler');
+    this.setState({ 
+      persons:[
+          { name: "Max", age: "28" },
+          { name: "Charles", age: newAge },
+          { name: "Stacy", age: Math.floor(Math.random() * 30)}
+      ] 
+    });
+  };
+
+  nameChangedHandler = (event) => {
+    console.log('nameChangedHandler');
+    this.setState({ 
+      persons:[
+          { name: "Max", age: "28" },
+          { name: event.target.value, age:"32" },
+          { name: "Stacy", age: "22"}
+      ] 
+    });
+  }
+  
   render() {
+    // inline styling
+    const style = {
+      backgroundColor: 'white',
+      font: 'inherit',
+      border: '1px solid blue',
+      padding: '8px',
+      cursor: 'pointer'
+    }
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        {/* Using anonymous function be less performant than bind, should be avoided */}
+        <button 
+        style={style}
+        onClick={() => this.switchNameHandler(Math.floor(Math.random() * 30))} >Switch Name</button> 
+        <Person 
+          name={this.state.persons[0].name} 
+          age={this.state.persons[0].age} />
+        <Person 
+          name={this.state.persons[1].name} 
+          age={this.state.persons[1].age} 
+          click={this.switchNameHandler.bind(this, 30)} 
+          changed={this.nameChangedHandler} >My hobbies: Racing</Person>
+        <Person 
+          name={this.state.persons[2].name} 
+          age={this.state.persons[2].age} />
       </div>
     );
   }
